b97227cf8614514232ec3187145182985b00e9fa
annarodionova06-sudo <anna.rodionova06@gmail.com>
2025-11-10 10:40:39 +0300

    Update Triangle.kt

--- .github/workflows/Triangle.kt
+++ .github/workflows/Triangle.kt
@@ -1,1 +1,12 @@
-
+import kotlin.math.sqrt
+import kotlin.math.pow
+class Triangle {
+    val A = Point()
+    val B = Point()
+    val C = Point()
+    
+    fun inputTriangle() {
+        A.inputPoint("A")
+        B.inputPoint("B") 
+        C.inputPoint("C")
+    }

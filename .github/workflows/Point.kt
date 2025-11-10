dba4d3d912f4680f44dc832a52bad1c24ff4add4
annarodionova06-sudo <anna.rodionova06@gmail.com>
2025-11-10 10:30:16 +0300

    Update Point.kt

--- .github/workflows/Point.kt
+++ .github/workflows/Point.kt
@@ -1,36 +1,38 @@
-# This is a basic workflow to help you get started with Actions
+import kotlin.math.sqrt
 
-name: CI
+class Point {
+    // Свойство: координата X точки
+    var x: Double = 0.0
 
-# Controls when the workflow will run
-on:
-  # Triggers the workflow on push or pull request events but only for the "main" branch
-  push:
-    branches: [ "main" ]
-  pull_request:
-    branches: [ "main" ]
+    // Свойство: координата Y точки
+    var y: Double = 0.0
 
-  # Allows you to run this workflow manually from the Actions tab
-  workflow_dispatch:
+    // Метод для ввода координат точки с клавиатуры
+    fun inputPoint(pointName: String) {
+        while (true) {
+            println("Введите координаты точки $pointName (x y):")
+            try {
+                // Читаем ввод и разбиваем на части по пробелам
+                val input = readln().split(" ")
+                // Проверяем что введено ровно два числа
+                if (input.size != 2) {
+                    println("Ошибка! Введите две координаты через пробел")
+                    continue
+                }
+                // Преобразуем строки в числа и сохраняем в свойствах
+                x = input[0].toDouble()
+                y = input[1].toDouble()
+                break  // Выходим из цикла если все успешно
+            } catch (e: Exception) {
+                // Обрабатываем ошибку если введены не числа
+                println("Ошибка! Введите числа")
+            }
+        }
+    }
 
-# A workflow run is made up of one or more jobs that can run sequentially or in parallel
-jobs:
-  # This workflow contains a single job called "build"
-  build:
-    # The type of runner that the job will run on
-    runs-on: ubuntu-latest
-
-    # Steps represent a sequence of tasks that will be executed as part of the job
-    steps:
-      # Checks-out your repository under $GITHUB_WORKSPACE, so your job can access it
-      - uses: actions/checkout@v4
-
-      # Runs a single command using the runners shell
-      - name: Run a one-line script
-        run: echo Hello, world!
-
-      # Runs a set of commands using the runners shell
-      - name: Run a multi-line script
-        run: |
-          echo Add other actions to build,
-          echo test, and deploy your project.
+    fun distanceTo(other: Point): Double {
+        val dx = x - other.x
+        val dy = y - other.y
+        return sqrt(dx * dx + dy * dy)
+    }
+}
